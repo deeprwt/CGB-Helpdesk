b48941d27a708e3c1db29a9820165e597f1c20e9
deeprwt <rwtroshan28@gmail.com>
2026-01-21 12:03:17 +0530

    remove error table

--- src/app/(admin)/(others-pages)/(tables)/basic-tables/page.tsx
+++ src/app/(admin)/(others-pages)/(tables)/basic-tables/page.tsx
@@ -1,6 +1,6 @@
 import ComponentCard from "@/components/common/ComponentCard";
 import PageBreadcrumb from "@/components/common/PageBreadCrumb";
-import BasicTableOne from "@/components/tables/BasicTableOne";
+
 import { Metadata } from "next";
 import React from "react";
 
@@ -16,9 +16,7 @@ export default function BasicTables() {
     <div>
       <PageBreadcrumb pageTitle="Basic Table" />
       <div className="space-y-6">
-        <ComponentCard title="Basic Table 1">
-          <BasicTableOne />
-        </ComponentCard>
+
       </div>
     </div>
   );
